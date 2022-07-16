0ef590050ed0b513fba7dade87353ce73b4b140d
Lokplart <lokplart@gmail.com>
2022-07-16 16:47:17 +0300

    drop to scene

--- src/drag.gd
+++ src/drag.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
 var selected = false
+var inital_pos
+
+func _ready():
+	inital_pos = global_position
+	print(inital_pos)
+	print("=========")
 
 func _on_DiceBottom_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("click"):
@@ -9,8 +15,15 @@ func _on_DiceBottom_input_event(viewport, event, shape_idx):
 func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25*delta)
+		print(global_position)
 		
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and not event.pressed:
+		if event.button_index == BUTTON_LEFT and not event.pressed and selected:
 			selected = false
+			var distance = global_position.distance_to(inital_pos)
+			print(distance)
+			if distance < 55:
+				global_position = inital_pos
+			else:
+				self.visible = false
