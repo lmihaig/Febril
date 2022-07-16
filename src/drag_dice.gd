c14e33b7e9398ca6ba10475ec0bd6fb81a5f9c82
Lokplart <lokplart@gmail.com>
2022-07-16 16:18:05 +0300

    adding drag and drop for one dice

--- src/drag_dice.gd
+++
@@ -1,8 +0,0 @@
-extends TextureRect
-
-func _ready() -> void:
-	add_to_group("DRAGGABLE")
-	
-func get_drag_data(_position: Vector2):
-	return self
- 
--- src/target_drop.gd
+++
@@ -1,6 +0,0 @@
-extends Panel
-
-
-func can_drop_data(position: Vector2, data) -> bool:
-	var can_drop: bool = data is Node and data.is_in_group("DRAGGABLE")
-	return can_drop
