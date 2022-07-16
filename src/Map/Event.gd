6d9c1073b54affe84fb693ab94ab767a3a0eeb88
Licu Mihai <licu.mihai21@gmail.com>
2022-07-16 16:07:23 +0200

    added sprites to map

--- src/Map/Event.gd
+++ src/Map/Event.gd
@@ -4,13 +4,24 @@ const margin = 10
 
 var children: Array = []
 
+
+var texture = ImageTexture.new()
+
+	
+	
+func _ready():
+	texture = load("res://assets/sprites/fight.png")
+	
+
 func add_child_event(child):
 	if !children.has(child):
 		children.append(child)
 		update()
 
 func _draw():
-	draw_circle(Vector2.ZERO, 4, Color.whitesmoke)
+#	draw_circle(Vector2.ZERO, 4, Color.whitesmoke)
+
+	draw_texture(texture, -texture.get_size()/2)
 	
 	for child in children:
 		var line = child.position - position
