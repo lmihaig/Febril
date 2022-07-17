b6e9b57a5fa65e2de2396a09da06de76571c7622
Licu Mihai <licu.mihai21@gmail.com>
2022-07-17 17:26:14 +0200

    added money

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -3,6 +3,8 @@ extends TextureRect
 var shield=0
 var HP=1
 var maxHP=1
+var reward = 10
+
 
 func _ready():
 	pass 
@@ -21,6 +23,7 @@ func damage(value):
 	
 	HP = clamp(HP - value, 0, maxHP)
 	if(HP==0):
+		PlayerInfo.coins += reward
 		get_tree().change_scene("res://src/Map/Map.tscn")
 		# FUCKING DIE
 		
