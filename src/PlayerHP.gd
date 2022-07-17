7096672ffbeb57910e85012d001c3594878ff2e0
Licu Mihai <licu.mihai21@gmail.com>
2022-07-17 17:42:02 +0200

    combat_bg

--- src/PlayerHP.gd
+++
@@ -1,9 +0,0 @@
-extends ProgressBar
-
-onready var health_bar = $PlayerHP
-
-func _on_health_updated(health, amount):
-	health_bar.value = health
-
-func _on_max_health_updated(max_health):
-		health_bar.max_value = max_health
